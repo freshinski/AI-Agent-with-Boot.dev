5fe0cc970d253a000598002dc7f4b34f92945e4c
freshinski <max.rast96@gmail.com>
2026-01-11 17:57:52 +0100

    counting tokens in responses

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
+
+if response.usage_metadata == None:
+    raise RuntimeError("Response usage metadata is None.")
+else:
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
 print(response.text)
 
 def main():
